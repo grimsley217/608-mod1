cc0e8b18d191250270b69f537b9d01a44fc3a982
grimsley217 <84026008+grimsley217@users.noreply.github.com>
2021-05-12 07:54:42 -0500

    bonus.py

---
+++ bonus.py
@@ -0,0 +1,7 @@
+#bonus.py
+"""This takes a string of 10 integeres and finds/prints the min, max, and range."""
+
+x=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print('The minimum is', min(x), '.')
+print('The maxiumum is', max(x), '.')
+print('The range is', min(x), '-', max(x), '.')
